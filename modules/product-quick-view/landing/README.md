87c32f2b890adf46ccbe009f81b732188702ef7c
Daxesh Gandhi <daxesh.gandhi@klevu.com>
2019-09-26 15:50:12 +0530

    Product Quick View module for landing page

---
+++ modules/product-quick-view/landing/README.md
@@ -0,0 +1,33 @@
+# Product Quick View - Search Results Landing Page
+
+>**Prerequisite:**  
+>This module uses [add-to-cart](/components/add-to-cart) base component. Before starting the implementation of the module, add base component files and include initialization.  
+
+![Quick-view product-grid](/modules/product-quick-view/images/product-grid.png)
+![Quick-view template](/modules/product-quick-view/images/product-quick-view.png) 
+
+You will find the necessary resources for this module available here:
+[resources](/modules/product-quick-view/landing/resources). Please add these with the
+method appropriate to your chosen framework. 
+
+This module will extend the [addToCart](/components/add-to-cart) component. So, you need to first add the below prerequisites first and then add the module resources.
+
+[klevu-landing-product-quick-view.js](/modules/product-quick-view/landing/resources/assets/js/klevu-landing-product-quick-view.js) It extends the base component and add functions related to quick view functionality. Also, it has the code for adding functionality to a quick view as well as usage of component extension.
+
+## Template Modifications
+
+Add the template for rendering Quick view for related results,
+so edit the corresponding `landing-product-block.tpl` to add a Quick view renderer template.
+
+```html
+...
+<div class="kuProdTop">
+    ...
+    <div class="kuQuickView">
+        <button data-id="<%=dataLocal.id%>" class="kuQuickViewBtn">Quick view</button>
+    </div>
+</div>
+...
+```
+
+Now, try **Quick view**!
---
+++ modules/product-quick-view/landing/resources/assets/js/klevu-landing-product-quick-view.js
@@ -0,0 +1,230 @@
+/**
+ * Extend addToCart base module for Quick view
+ */
+
+klevu.extend({
+    addToCartQuickView: function (mainScope) {
+
+        if (!mainScope.addToCart) {
+            console.log("Add to cart base module is missing!");
+            return;
+        }
+
+        mainScope.addToCart.quickView = {
+
+            /*
+             *	Function to toggle Body scroll style
+             */
+            toggleBodyScroll: function () {
+                var body = klevu.dom.find("body")[0];
+                var isScroll = '';
+                if (!body.style.overflow) {
+                    isScroll = "hidden";
+                }
+                body.style.overflow = isScroll;
+            },
+
+            /*
+             *	Function to toggle modal UI
+             */
+            toggleModal: function () {
+                this.toggleBodyScroll();
+                var modalElement = klevu.dom.find("div.kuModal");
+                if (modalElement.length) {
+                    modalElement[0].classList.toggle("show-modal");
+                }
+            },
+
+            /**
+             * Function to attach add to cart button event
+             * @param {*} event 
+             */
+            attachKlevuQuickViewAddToCartBtnEvent: function (event) {
+                event = event || window.event;
+                event.preventDefault();
+                var selected_product;
+                var target = klevu.dom.find(".productQuickView");
+                if (target && target[0]) {
+                    selected_product = target[0].selected_product;
+                }
+                if (selected_product) {
+                    mainScope.addToCart.base.sendAddToCartRequest(selected_product.id, 1);
+                }
+                this.toggleModal();
+            },
+
+            /**
+             * Function to bind add to cart button event
+             */
+            bindAddToCartEvent: function () {
+                var self = this;
+                var addToCartElement = klevu.dom.find(".kuModalProductCart", ".productQuickView");
+                if (addToCartElement.length) {
+                    klevu.event.attach(addToCartElement[0], "click", function (event) {
+                        self.attachKlevuQuickViewAddToCartBtnEvent(event);
+                    });
+                }
+            }
+        };
+    }
+});
+
+/**
+ * Quick view service file
+ */
+klevu.extend({
+    quickViewService: function (mainScope) {
+        mainScope.quickViewService = {
+            modal: null,
+            closeButton: null,
+            selected_product: null,
+            /*
+             *	Add container for Product Quick view
+             */
+            appendTemplateIntoBody: function () {
+                var quickViewCont = document.createElement("div");
+                quickViewCont.className = "quickViewWrap productQuickView";
+                window.document.body.appendChild(quickViewCont);
+            },
+            /*
+             *	Function to toggle Body scroll style
+             */
+            toggleBodyScroll: function () {
+                var body = klevu.dom.find("body")[0];
+                var isScroll = '';
+                if (!body.style.overflow) {
+                    isScroll = "hidden";
+                }
+                body.style.overflow = isScroll;
+            },
+            /*
+             *	Function to toggle modal UI
+             */
+            toggleModal: function () {
+                this.toggleBodyScroll();
+                var modalElement = klevu.dom.find("div.kuModal", '.productQuickView');
+                if (modalElement.length) {
+                    this.modal = modalElement[0];
+                    this.modal.classList.toggle("show-modal");
+                }
+            },
+            /*
+             *	Function to fire on window click event to hide modal
+             */
+            windowOnClick: function (event) {
+                if (event.target === this.modal) {
+                    this.toggleModal();
+                }
+            },
+            /**
+             * Function to bind event on close button
+             */
+            bindCloseBtnClick: function () {
+                var self = this;
+                var closeElement = klevu.dom.find(".close-button", '.productQuickView');
+                if (closeElement.length) {
+                    self.closeButton = closeElement[0];
+                    klevu.event.attach(self.closeButton, "click", function () {
+                        self.toggleModal();
+                    });
+                }
+            },
+            /**
+             * Landing page onload event
+             * @param {*} data 
+             * @param {*} scope 
+             */
+            landingPageTemplateOnLoadEvent: function (data) {
+                var self = this;
+                var target = klevu.getSetting(mainScope.settings, "settings.search.searchBoxTarget");
+                klevu.each(klevu.dom.find(".kuQuickViewBtn", target), function (key, value) {
+                    klevu.event.attach(value, "click", function (event) {
+                        event = event || window.event;
+                        event.preventDefault();
+                        var selected_product_id = (this.getAttribute("data-id")) ? this.getAttribute("data-id") : null;
+                        var items = klevu.getObjectPath(data.template.query, 'productList');
+                        if (items.result) {
+                            klevu.each(items.result, function (key, value) {
+                                if (value.id == selected_product_id) {
+                                    selected_product = value;
+                                }
+                            })
+                        }
+                        data.template.selected_product = selected_product;
+                        var target = klevu.dom.find(".productQuickView");
+                        if (target && target[0]) {
+                            target[0].selected_product = selected_product;
+                        }
+                        klevu.event.fireChain(mainScope, "chains.quickView", mainScope.element, mainScope.data, event);
+                        self.toggleModal();
+                    });
+                });
+            }
+        };
+    }
+});
+
+/**
+ * Add Product Quick view functionality
+ */
+klevu.coreEvent.attach("setRemoteConfigLanding", {
+    name: "product-quick-view",
+    fire: function () {
+
+        /** Include addToCart base module first to use base functionalities */
+        klevu.addToCart(klevu.search.landing.getScope().element.kScope);
+
+        /** Initalize add to cart service */
+        klevu.addToCartQuickView(klevu.search.landing.getScope().element.kScope);
+
+
+        /** Set template in landing UI */
+        klevu.search.landing.getScope().template.setTemplate(klevu.dom.helpers.getHTML("#klevuLandingTemplateQuickView"), "quick-view", true);
+
+        /** Create chain for Quick view */
+        klevu.search.landing.getScope().chains.quickView = klevu.chain();
+
+        /** Initialize Quick view service */
+        klevu.quickViewService(klevu.search.landing.getScope().element.kScope);
+
+        /** Add Quick view wrapper container in body */
+        klevu.search.landing.getScope().quickViewService.appendTemplateIntoBody();
+
+
+        /*
+         *	Add Quick view template and update data into that
+         */
+        klevu.search.landing.getScope().chains.quickView.add({
+            name: "chainQuickView",
+            fire: function (data, scope) {
+                event = event || window.event;
+                event.preventDefault();
+                scope.kScope.template.setData(data.template);
+                var target = klevu.dom.find("div.quickViewWrap")[0];
+                target.innerHTML = '';
+                var element = scope.kScope.template.convertTemplate(scope.kScope.template.render("quick-view"));
+                scope.kScope.template.insertTemplate(target, element);
+
+                klevu.search.landing.getScope().quickViewService.bindCloseBtnClick();
+                klevu.search.landing.getScope().addToCart.quickView.bindAddToCartEvent();
+            }
+        });
+
+        /**
+         *  Bind body click event
+         */
+        klevu.event.attach(window, "click", function (event) {
+            klevu.search.landing.getScope().quickViewService.windowOnClick(event);
+        });
+
+        /*
+         *	Bind and handle click event on Quick view button 
+         */
+        klevu.search.landing.getScope().chains.template.events.add({
+            name: "quickViewButtonClick",
+            fire: function (data, scope) {
+                klevu.search.landing.getScope().quickViewService.landingPageTemplateOnLoadEvent(data);
+            }
+        });
+    }
+})
