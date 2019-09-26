477e197292efa5e93f6a321f77fe929e1c48dab9
Daxesh Gandhi <daxesh.gandhi@klevu.com>
2019-09-26 11:56:49 +0530

    Add to cart module for quick search

--- modules/add-to-cart/quick/resources/assets/js/klevu-quick-add-to-cart.js
+++ modules/add-to-cart/quick/resources/assets/js/klevu-quick-add-to-cart.js
@@ -65,22 +65,23 @@ klevu.extend({
 klevu.coreEvent.attach("setRemoteConfigQuick", {
     name: "addAddToCartButtonQuickSearch",
     fire: function () {
+        klevu.each(klevu.search.extraSearchBox, function (key, box) {
+            /** Initialize add to cart base module in quick search */
+            klevu.addToCart(box.getScope().element.kScope);
 
-        /** Include addToCart base module first to use base functionalities */
-        klevu.addToCart(klevu.search.quick.getScope().element.kScope);
+            /** Set Template */
+            box.getScope().template.setTemplate(klevu.dom.helpers.getHTML("#quickSearchProductAddToCart"), "quickSearchProductAddToCart", true);
 
-        /** Set Template */
-        klevu.search.quick.getScope().template.setTemplate(klevu.dom.helpers.getHTML("#quickSearchProductAddToCart"), "quickSearchProductAddToCart", true);
+            /** Initalize add to cart service */
+            klevu.addToCartQuick(box.getScope().element.kScope);
 
-        /** Initalize add to cart service */
-        klevu.addToCartQuick(klevu.search.quick.getScope().element.kScope);
-
-        /** Bind quick page add to cart button click event */
-        klevu.search.quick.getScope().chains.template.events.add({
-            name: "quickSearchProductAddToCartEvent",
-            fire: function (data, scope) {
-                klevu.search.quick.getScope().addToCart.quick.bindQuickSearchProductAddToCartBtnClickEvent(data, "productList");
-            }
+            /** Bind quick page add to cart button click event */
+            box.getScope().chains.template.events.add({
+                name: "quickSearchProductAddToCartEvent",
+                fire: function (data, scope) {
+                    scope.kScope.addToCart.quick.bindQuickSearchProductAddToCartBtnClickEvent(data, "productList");
+                }
+            });
         });
     }
 });
