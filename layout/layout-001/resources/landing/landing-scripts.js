cd93bea4fe28abacac833dc9b0b7f82133eb4b21
Daxesh Gandhi <daxesh.gandhi@klevu.com>
2019-11-27 16:41:37 +0530

    collapsable filter module restructure

--- layout/layout-001/resources/landing/landing-scripts.js
+++ layout/layout-001/resources/landing/landing-scripts.js
@@ -1310,35 +1310,100 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
 /**
  * Extension for collapse filter functionality
  */
-klevu.extend({
-    collapseFilters: function (mainScope) {
-        mainScope.collapseFilters = {};
-        mainScope.collapseFilters.base = {
-            /**
-             * Function to collapse filter list as per the priority list
-             * @param {*} data 
-             * @param {*} collapsedFilters 
-             */
-            collapse: function (data, collapsedFilters) {
-                if (data && data.template && data.template.query && collapsedFilters) {
-                    var items = klevu.getObjectPath(data.template.query, 'productList');
-                    if (items && items.filters) {
-                        var filters = items.filters;
-                        filters.forEach(function (filter) {
-                            filter.isCollapsed = false;
-                        });
-                        collapsedFilters.forEach(function (collapsedFilter) {
-                            filters.forEach(function (filter) {
-                                if (collapsedFilter.key == filter.key) {
-                                    filter.isCollapsed = true;
-                                }
-                            });
-                        });
-                    }
-                }
+
+klevu.interactive(function () {
+
+    /**
+     * Initialize collapsing for filter items
+     */
+    function initialize(scope) {
+        var target = klevu.getSetting(scope.settings, "settings.search.searchBoxTarget");
+        //add collapsable filters
+        klevu.each(klevu.dom.find(".kuFilterHead", target), function (key, value) {
+            // onclick
+            klevu.event.attach(value, "click", function (event) {
+                event = event || window.event;
+                event.preventDefault();
+                
+                this.classList.toggle("kuCollapse");
+                this.classList.toggle("kuExpand");
+
+                var parentElem = klevu.dom.helpers.getClosest(this, ".kuFilterBox");
+                klevu.each(klevu.dom.find(".kuFilterNames", parentElem), function (key, value) {
+                    value.classList.toggle("kuFilterCollapse");
+                    value.classList.remove("kuFilterShowAll");
+                });
+
+            });
+        });
+        //add expandable filters
+        klevu.each(klevu.dom.find(".kuShowOpt", target), function (key, value) {
+            // onclick
+            klevu.event.attach(value, "click", function (event) {
+                event = event || window.event;
+                event.preventDefault();
+
+                var parentElem = klevu.dom.helpers.getClosest(this, ".kuFilterBox");
+                klevu.each(klevu.dom.find(".kuFilterNames", parentElem), function (key, value) {
+                    value.classList.toggle("kuFilterShowAll");
+                });
+
+            });
+        });
+    };
+
+    /**
+     * Function to collapse filter list as per the priority list
+     * @param {*} data 
+     * @param {*} collapsedFilters 
+     */
+    function collapse(data, collapsedFilters, itemListId) {
+        if (data && data.template && data.template.query && collapsedFilters && collapsedFilters.length) {
+            var items = klevu.getObjectPath(data.template.query, itemListId);
+            if (items && items.filters) {
+                var filters = items.filters;
+                filters.forEach(function (filter) {
+                    filter.isCollapsed = false;
+                });
+                collapsedFilters.forEach(function (collapsedFilter) {
+                    filters.forEach(function (filter) {
+                        if (collapsedFilter.key == filter.key) {
+                            filter.isCollapsed = true;
+                        }
+                    });
+                });
             }
-        };
-    }
+        }
+    };
+
+    var collapseFilters = {
+        collapse: collapse,
+        initialize: initialize
+    };
+
+    klevu.extend(true, klevu.search.modules, {
+        collapseFilters: {
+            base: collapseFilters,
+            build: true
+        }
+    });
+});
+
+/**
+ * collapseFilters module build event
+ */
+klevu.coreEvent.build({
+    name: "collapseFiltersModuleBuild",
+    fire: function () {
+        if (!klevu.search.modules ||
+            !klevu.search.modules.collapseFilters ||
+            !klevu.search.modules.collapseFilters.build) {
+            return false;
+        }
+        return true;
+    },
+    maxCount: 500,
+    delay: 30
 });
 
 /**
@@ -1348,11 +1413,18 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
     name: "collapseFilters",
     fire: function () {
 
-        /** Initialize collapseFilters module */
-        klevu.collapseFilters(klevu.search.landing.getScope().element.kScope);
+        /**
+         * Function to enable collapsing for filter items
+         */
+        klevu.search.landing.getScope().chains.template.events.add({
+            name: "enableFilterCollapse",
+            fire: function (data, scope) {
+                klevu.search.modules.collapseFilters.base.initialize(scope.kScope);
+            }
+        });
 
         /**
-         * Function to set filter priority list and reoder filter list
+         * Function to set filter priority list and reorder filter list
          */
         klevu.search.landing.getScope().chains.template.render.addBefore("renderResponse", {
             name: "collapseFilterPosition",
@@ -1369,7 +1441,7 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
                         key: "product_type"
                     }];
 
-                    klevu.search.landing.getScope().collapseFilters.base.collapse(data, collapsedFilters);
+                    klevu.search.modules.collapseFilters.base.collapse(data, collapsedFilters, 'productList');
                 }
             }
         });
