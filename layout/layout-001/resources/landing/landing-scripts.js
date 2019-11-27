ec8757ae208393b9fc35996d0a099d0aa6711e5a
Daxesh Gandhi <daxesh.gandhi@klevu.com>
2019-11-27 17:54:40 +0530

    Filter reorder module restructure

--- layout/layout-001/resources/landing/landing-scripts.js
+++ layout/layout-001/resources/landing/landing-scripts.js
@@ -1752,42 +1752,68 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
 /**
  * Extension for reordering filters
  */
-klevu.extend({
-    reorderFilters: function (mainScope) {
-        mainScope.reorderFilters = {};
-        mainScope.reorderFilters.base = {
-            /**
-             * Function to reorder filter list as per the priority list
-             * @param {*} data 
-             * @param {*} priorityFilters 
-             */
-            reorder: function (data, priorityFilters) {
-                if (data && data.template && data.template.query) {
-                    var items = klevu.getObjectPath(data.template.query, 'productList');
-                    if (items && items.filters) {
-                        var filters = items.filters;
-                        var otherIndexStart = priorityFilters.length + 1;
-                        priorityFilters.forEach(function (priorityFilter, index) {
-                            filters.forEach(function (filter) {
-                                if (filter.key == priorityFilter.key) {
-                                    filter.sort = index + 1;
-                                }
-                            });
-                        });
-                        filters.forEach(function (filter) {
-                            if (!filter.sort) {
-                                filter.sort = otherIndexStart;
-                                otherIndexStart++;
-                            }
-                        });
-                        filters.sort(function (a, b) {
-                            return a.sort - b.sort;
-                        });
+
+klevu.interactive(function () {
+
+    /**
+     * Function to reorder filter list as per the priority list
+     * @param {*} data 
+     * @param {*} priorityFilters 
+     * @param {*} dataListName 
+     */
+    function reorder(data, priorityFilters, dataListName) {
+        if (data && data.template && data.template.query) {
+            var items = klevu.getObjectPath(data.template.query, dataListName);
+            if (items && items.filters) {
+                var filters = items.filters;
+                var otherIndexStart = priorityFilters.length + 1;
+                priorityFilters.forEach(function (priorityFilter, index) {
+                    filters.forEach(function (filter) {
+                        if (filter.key == priorityFilter.key) {
+                            filter.sort = index + 1;
+                        }
+                    });
+                });
+                filters.forEach(function (filter) {
+                    if (!filter.sort) {
+                        filter.sort = otherIndexStart;
+                        otherIndexStart++;
                     }
-                }
+                });
+                filters.sort(function (a, b) {
+                    return a.sort - b.sort;
+                });
             }
-        };
+        }
     }
+
+    var reorderFilters = {
+        reorder: reorder
+    };
+
+    klevu.extend(true, klevu.search.modules, {
+        reorderFilters: {
+            base: reorderFilters,
+            build: true
+        }
+    });
+});
+
+/**
+ * reorderFilters module build event
+ */
+klevu.coreEvent.build({
+    name: "reorderFiltersModuleBuild",
+    fire: function () {
+        if (!klevu.search.modules ||
+            !klevu.search.modules.reorderFilters ||
+            !klevu.search.modules.reorderFilters.build) {
+            return false;
+        }
+        return true;
+    },
+    maxCount: 500,
+    delay: 30
 });
 
 /**
@@ -1797,11 +1823,8 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
     name: "reorderFilters",
     fire: function () {
 
-        /** Initialize reorderFilters */
-        klevu.reorderFilters(klevu.search.landing.getScope().element.kScope);
-
         /**
-         * Function to set filter priority list and reoder filter list
+         * Function to set filter priority list and reorder filter list
          */
         klevu.search.landing.getScope().chains.template.render.addBefore("renderResponse", {
             name: "reorderFilterPosition",
@@ -1812,7 +1835,7 @@ klevu.coreEvent.attach("setRemoteConfigLanding", {
                     }, {
                         key: "category"
                     }];
-                    klevu.search.landing.getScope().reorderFilters.base.reorder(data, priorityFilters);
+                    klevu.search.modules.reorderFilters.base.reorder(data, priorityFilters, 'productList');
                 }
             }
         });
