e71d0e1e249b9761f0654a53bd0fb210fce04f08
Daxesh Gandhi <daxesh.gandhi@klevu.com>
2020-05-06 17:58:47 +0530

    Added Product Recommendation Modules

---
+++ components/product-recommendation/README.md
@@ -0,0 +1,20 @@
+# Product Recommendation - Base Component
+
+> **Note:**  
+> This is a base component for product recommendation modules.
+> It won't have any output on user interface. It only initializes core functions.
+
+You will find the necessary resources for this component available here:
+[resources](/components/product-recommendation/resources). Please add these with the
+method appropriate to your chosen framework.
+
+[Base file](/components/product-recommendation/resources/assets/js/klevu-product-recommendation.js) contains the core logic of the functionality.
+
+```javascript
+/** Usage of base component functions */
+klevu.search.modules.productsRecommendation.base.FUNCTION_NAME();
+```
+
+Try accessing the `productsRecommendation` base component.
+
+**Reference modules based on this component:**
---
+++ components/product-recommendation/resources/assets/js/klevu-product-recommendation.js
@@ -0,0 +1,158 @@
+/**
+ * Base component for Product Recommendation Module
+ */
+(function (klevu) {
+
+    /**
+     * Function to add Record query object
+     * @param {*} scope 
+     * @param {*} requestQuery 
+     */
+    function addRecordQueryObject(scope, requestQuery) {
+        scope.chains.request.build.add({
+            name: "addProductRecommendationReq",
+            fire: function (data, scope) {
+                data.request.current.recordQueries.push(requestQuery);
+                data.context.doSearch = true;
+                data.context.section = requestQuery.id;
+            }
+        });
+    }
+
+    /**
+     * Function to render module response 
+     * @param {*} scopeVariable 
+     * @param {*} appendToClass 
+     * @param {*} templateElement 
+     */
+    function renderResponse(scopeVariable, appendToClass, templateElement) {
+        var scope = scopeVariable.getScope();
+        scope.chains.template.render.add({
+            name: "renderResponse",
+            fire: function (data, scope) {
+                if (data.context.isSuccess) {
+                    scopeVariable.setTarget(klevu.dom.find(appendToClass)[0]);
+                    scope.kScope.template.setData(data.template);
+                    var targetBox = templateElement;
+                    var element = scope.kScope.template.convertTemplate(scope.kScope.template.render(targetBox));
+                    var target = klevu.dom.find(appendToClass)[0];
+                    if (target) {
+                        target.innerHTML = '';
+                        scope.kScope.element.kData = data.template;
+                        scope.kScope.template.insertTemplate(target, element);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Function to fire module chain
+     * @param {*} scope 
+     */
+    function fireChain(scope) {
+        klevu.setSetting(scope.settings, "settings.search.fullPageLayoutEnabled", true);
+        klevu.setSetting(scope.settings, "settings.search.minChars", 0);
+        var tempElement = scope.element;
+        tempElement.kScope.data = tempElement.kObject.resetData(tempElement);
+        klevu.event.fireChain(tempElement.kScope, "chains.events.keyUp", tempElement, tempElement.kScope.data, null);
+    }
+
+    /**
+     * Function to process product currency
+     * @param {*} scope 
+     */
+    function currencyProcess(scope) {
+        var scopeCurrency = scope.getScope().template.getTranslator().getCurrencyObject();
+        scopeCurrency.setCurrencys({
+            'GBP': {
+                string: "£",
+                format: "%s%s",
+                atEnd: false,
+                precision: 2,
+                thousands: ",",
+                decimal: ".",
+                grouping: 3
+            },
+            'USD': {
+                string: "USD",
+                atEnd: true
+            },
+            'EUR': {
+                string: "EUR",
+                format: "%s %s",
+                atEnd: true
+            },
+        });
+        scopeCurrency.mergeToGlobal();
+    }
+
+    /**
+     * Function to create and get scope 
+     * @param {*} moduleName 
+     */
+    function createAndGetScope(moduleName) {
+        klevu.search[moduleName] = klevu.searchObjectClone(klevu.search.base);
+        currencyProcess(klevu.search[moduleName]);
+        return klevu.search[moduleName];
+    }
+
+    /**
+     * Function to get the product ids
+     * @param {*} parameterProductIds 
+     * @param {*} globalVariableProductIds 
+     */
+    function getProductIds(parameterProductIds, globalVariableProductIds) {
+        var productIds = [];
+        var tempProductIds = [];
+
+        if (parameterProductIds && parameterProductIds.length) {
+            tempProductIds = parameterProductIds;
+        } else if (globalVariableProductIds && globalVariableProductIds.length) {
+            tempProductIds = globalVariableProductIds;
+        }
+
+        if (tempProductIds.length) {
+            tempProductIds.forEach(productId => {
+                productIds.push({
+                    "id": productId
+                });
+            });
+        }
+
+        return productIds;
+    }
+
+    var productsRecommendation = {
+        addRecordQueryObject: addRecordQueryObject,
+        renderResponse: renderResponse,
+        fireChain: fireChain,
+        createAndGetScope: createAndGetScope,
+        getProductIds: getProductIds,
+        currencyProcess: currencyProcess
+    };
+
+    klevu.extend(true, klevu.search.modules, {
+        productsRecommendation: {
+            base: productsRecommendation,
+            build: true
+        }
+    });
+
+})(klevu);
+
+klevu.coreEvent.build({
+    name: "productsRecommendationModule",
+    fire: function () {
+        if (!klevu.search.modules ||
+            !klevu.search.modules.productsRecommendation ||
+            !klevu.search.modules.productsRecommendation.build ||
+            !klevu.getSetting(klevu.settings, "settings.localSettings", false) ||
+            klevu.isUndefined(klevu.search.landing)) {
+            return false;
+        }
+        return true;
+    },
+    maxCount: 500,
+    delay: 30
+});
